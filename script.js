6263b00fe415616d4e28760f8adfb96ab7d17685
lasha-gl <gegechkori19@gmail.com>
2021-10-06 15:13:15 +0400

    add

---
+++ script.js
@@ -0,0 +1,69 @@
+// #1
+// let cars = ['audi', 'bmw', 'lexus', 'vokswagen', 'porsche', 'audi']
+
+// for (let i=1; i<cars.length; i++) {
+//     console.log(cars[i]); 
+// }
+
+// #2
+// let names = ['ani', 'lasha', 'taso', 'aleqsi']
+// for (let i=0; i < names.length; i++) {
+//     if (names[i].length > 3) {
+//         console.log(names[i]);
+//     }
+// }
+
+// #3 ლუწი
+
+// let numbers = [2, 3, 5, 9, 10, 20];
+
+// for (let i=0; i < numbers.length; i++) {
+//     if (numbers [i] % 2 == 0) {
+//         console.log(numbers[i]);
+//     }
+// }
+
+
+// #3 კენტი
+
+// let numbers = [2, 3, 5, 9, 10, 20];
+
+// for (let i=0; i < numbers.length; i++) {
+//     if (numbers [i] % 2 == 1) {
+//         console.log(numbers[i]);
+//     }
+// }
+
+
+// #4
+// let numbers = [15, 67, 100, 5, 7, [80,500]];
+
+// console.log(numbers[5][1]);
+
+// #5
+// var person = {
+//     firstname: 'john',
+//     lastname: 'doe',
+//     age: 50,
+//     eyecolor: 'blue'
+// }
+// console.log(person.eyecolor);
+
+// #6
+// let names = ['ani', 'lasha', 'taso', 'aleqsi']
+
+// names.forEach(function(item) {
+//     if (item.length){
+//         console.log(item);
+//     }
+// });
+
+// #7 ნიკას მაგალითი
+// var names = ['ani', 'lasha', 'taso', 'aleqsi', 'nika']
+// if(names.length===5 || names[names.lenght-1]==='nika')
+// console.log('მასივის რაოდენობა: 5 და ბოლო ელემენტია ნიკა.');
+
+// 7 სანდროს მაგალითი
+// var names = ['sandro', 'dato', 'ani', 'lasha', 'taso', 'aleqsi', 'nika']
+// if (names.length===7 || names[0]==='sandro')
+// console.log('მასივის რაოდენობა: 7 და პირველი ელემენტია სანდრო.');
